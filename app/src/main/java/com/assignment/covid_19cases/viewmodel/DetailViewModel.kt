363902a8b9076bc768a8480a2310279dc540cec0
Your Name <you@example.com>
2021-12-06 01:11:39 +0530

    Formated Number Format

--- app/src/main/java/com/assignment/covid_19cases/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/assignment/covid_19cases/viewmodel/DetailViewModel.kt
@@ -10,6 +10,8 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.net.UnknownHostException
+import java.text.NumberFormat
+import java.util.*
 
 
 private const val TAG = "DetailViewModel"
@@ -17,6 +19,10 @@ class DetailViewModel(private val countryCode:String) :ViewModel(){
     private val apiService by lazy { ApiService.getInstance() }
     private val _countryDetail=MutableLiveData<CountryCases>()
     val countryDetail get() = _countryDetail
+    val death:LiveData<String> = Transformations.switchMap(_countryDetail,::getDeaths)
+    val recovered:LiveData<String> = Transformations.switchMap(_countryDetail,::getRecovered)
+    val getActiveCase:LiveData<String> = Transformations.switchMap(_countryDetail,::getActiveCase)
+    val getCritical:LiveData<String> = Transformations.switchMap(_countryDetail,::getCritical)
     val getLastUpdateTime=Transformations.switchMap(_countryDetail,::getUpdatedTime)
     val getLastChangedTime=Transformations.switchMap(_countryDetail,::getChangedTime)
 
@@ -55,8 +61,33 @@ class DetailViewModel(private val countryCode:String) :ViewModel(){
         }
     }
 
+    private fun getDeaths(countryCases: CountryCases): LiveData<String> {
+        return MutableLiveData(NumberFormat.getNumberInstance(Locale.US).format(
+            countryCases.deaths.toInt()
+        ).toString())
+    }
+
+    private fun getRecovered(countryCases: CountryCases): LiveData<String> {
+        return MutableLiveData(NumberFormat.getNumberInstance(Locale.US).format(
+            countryCases.recovered.toInt()
+        ).toString())
+    }
+
+    private fun getActiveCase(countryCases: CountryCases): LiveData<String> {
+        return MutableLiveData(NumberFormat.getNumberInstance(Locale.US).format(
+            countryCases.confirmed.toInt()
+        ).toString())
+    }
+    private fun getCritical(countryCases: CountryCases): LiveData<String> {
+        return MutableLiveData(NumberFormat.getNumberInstance(Locale.US).format(
+            countryCases.critical.toInt()
+        ).toString())
+    }
+
 
 
 
 
 }
+
+
